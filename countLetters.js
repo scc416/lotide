1f3624871b019e5664f3912592f9453761728986
Siu <siuc@protonmail.com>
2021-11-01 22:58:38 +0000

    eslint countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,14 +1,16 @@
 const countLetters = (str) => {
   let numOfLetter = {};
   // str = str.toLowerCase();
-  for(const char of str.toLowerCase()) {
-    if (char !== " ") {
-      numOfLetter.hasOwnProperty(char) 
-      ? numOfLetter[char] ++ 
-      : numOfLetter[char] = 1 ;
+  for (const char of str.toLowerCase()) {
+    const charIsSpace = char === " ";
+    if (!charIsSpace) {
+      const charHasDefinedAsKey = char in numOfLetter;
+      charHasDefinedAsKey
+        ? numOfLetter[char] ++
+        : numOfLetter[char] = 1;
     }
   }
   return numOfLetter;
-}
+};
 
 module.exports = countLetters;
