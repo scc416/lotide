b87eddd42800b6f015ac7da97eac656bab1a4bc2
Siu <siuc@protonmail.com>
2021-11-01 18:06:53 +0000

    refector assertArraysEqual.js with new file assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,11 @@
-const eqArrays = (arr1, arr2) => {
-  // compare length of the arrays
-  if (arr1.length !== arr2.length) return false;
-  for (let i in arr1) {
-    // compare each item in the array
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays.js');
 
 const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
+  const arraysAreEqual = eqArrays(arr1, arr2);
+  if (arraysAreEqual) {
     return console.log(`✅✅✅ Assertion Passed: [${arr1}] and [${arr2}] are equal.`);
-  } 
+  }
     return console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] and [${arr2}] are NOT equal.`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2], [1, 2, 3]);
-assertArraysEqual([1, 2, 4], [1, 2, 3]);
+module.exports = assertArraysEqual;
