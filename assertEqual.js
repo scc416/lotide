ca637f438563f66fac1c6bad0cd0f40f94a48864
Siu <siuc@protonmail.com>
2021-11-01 22:17:47 +0000

    refactor assertEqual.js and remove module.exports

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,8 @@
 const assertEqual = function(actual, expected) {
   const asExpected = actual === expected;
-  if (asExpected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  const eqMessage = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  const diffMessage = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  if (asExpected) return console.log(eqMessage);
+  if (!asExpected) return console.log(diffMessage);
   }
 };
-
-module.exports = assertEqual;
