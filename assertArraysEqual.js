b5f5e23d5f86094299c5f1d4e6921c8f7c98318d
Siu <siuc@protonmail.com>
2021-11-01 22:27:53 +0000

    eslint assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,6 +2,10 @@ const eqArrays = require('./eqArrays.js');
 
 const assertArraysEqual = (arr1, arr2) => {
   const arraysAreEqual = eqArrays(arr1, arr2);
-  if (arraysAreEqual) return console.log(`✅✅✅ Assertion Passed: [${arr1}] and [${arr2}] are equal.`);
-  if (!arraysAreEqual) return console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] and [${arr2}] are NOT equal.`);
+  const eqMessage = `✅✅✅ Assertion Passed: [${arr1}] and [${arr2}] are equal.`;
+  const diffMessage = `🛑🛑🛑 Assertion Failed: [${arr1}] and [${arr2}] are NOT equal.`;
+  if (arraysAreEqual) return console.log(eqMessage);
+  if (!arraysAreEqual) return console.log(diffMessage);
 };
+
+module.exports = assertArraysEqual;
