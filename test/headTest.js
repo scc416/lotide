38d93c48d144a24343c1f98f60c5a0cfca9c1cef
Siu <siuc@protonmail.com>
2021-11-01 18:45:02 +0000

    add library mocha & chai, apply to headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,32 @@
 const head = require('../head.js');
-const assertEqual = require('../assertEqual.js');
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([5]), 5);
-assertEqual(head(["Hello"]), "Hello");
-assertEqual(head(["Labs"]), "Labs");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it(`returns "Hello" for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it(`returns "Hello" for ["Hello"]`, () => {
+    assert.strictEqual(head(["Hello"]), "Hello");
+  });
+
+});
