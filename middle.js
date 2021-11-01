c879d66d36bc508ab98649c429ae7fae3bd55c21
Siu <siuc@protonmail.com>
2021-11-01 18:12:56 +0000

    create middleTest.js to refactor middle.js

--- middle.js
+++ middle.js
@@ -34,9 +34,4 @@ const middle = function(array) {
   }
 }
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,9 @@
+const middle = require('../middle.js');
+const assertArraysEqual = require('../assertArraysEqual.js');
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1,2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
