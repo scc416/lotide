c31814ad808923d766ca299e45dafdc64ad6b089
Siu <siuc@protonmail.com>
2021-11-01 22:14:32 +0000

    refactor assertArraysEqual.js and remove module.exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,10 +2,6 @@ const eqArrays = require('./eqArrays.js');
 
 const assertArraysEqual = (arr1, arr2) => {
   const arraysAreEqual = eqArrays(arr1, arr2);
-  if (arraysAreEqual) {
-    return console.log(`✅✅✅ Assertion Passed: [${arr1}] and [${arr2}] are equal.`);
-  }
-    return console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] and [${arr2}] are NOT equal.`);
+  if (arraysAreEqual) return console.log(`✅✅✅ Assertion Passed: [${arr1}] and [${arr2}] are equal.`);
+  if (!arraysAreEqual) return console.log(`🛑🛑🛑 Assertion Failed: [${arr1}] and [${arr2}] are NOT equal.`);
 };
-
-module.exports = assertArraysEqual;
