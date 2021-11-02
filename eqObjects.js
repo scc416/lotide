8ca0a1fc18b6f8795162f6671d5922f041b639e4
scc416 <siuc@protonmail.com>
2021-11-02 04:19:16 +0000

    add eqArrays to eqObjects with require

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,4 @@
-const assertEqual = function(actual, expected) {
-  actual === expected
-    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-const eqArrays = (arr1, arr2) => {
-  // compare length of the arrays
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i in arr1) {
-    // compare each item in the array
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays.js")
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
@@ -24,8 +6,9 @@ const eqObjects = function(object1, object2) {
   const keys = Object.keys(object1);
   const length1 = keys.length;
   const length2 = Object.keys(object2).length;
-  if (length1 !== length2) return false;
-  for (let key of keys) {
+  const haveSameNumberOfKeys = length1 === length2;
+  if (!haveSameNumberOfKeys) return false;
+  for (const key of keys) {
     const keyExistsInObject2 = key in object2;
     if (!keyExistsInObject2) return false;
     const val1 = object1[key];
@@ -45,31 +28,5 @@ const eqObjects = function(object1, object2) {
   }
   return true;
 };
-
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
-
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),false); // => false
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { y: 0, z: 1 }, b: 2 }), true); // => true
-
-assertEqual(eqObjects({ a: { y: 0, z: {a: 1, b: 2} }, b: 2 }, { a: { y: 0, z: {a: 1, b: 2} }, b: 2 }), true); // => true
-assertEqual(eqObjects({ a: { y: 0, z: {a: 1, b: 2} }, b: 2 }, { a: { y: 0, z: {a: 3, b: 2} }, b: 2 }), false); // => false
-
-assertEqual(eqObjects({ a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }}, { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }}), true); // => true
-assertEqual(eqObjects({ a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }}, { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: ".", c: "-"} }, b: 2 }}), false); // => true
 
 module.exports = eqObjects;
