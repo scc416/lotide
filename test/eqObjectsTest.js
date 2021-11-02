d509438bf60556f2de1c3b293e82a2eb958b4406
scc416 <siuc@protonmail.com>
2021-11-02 04:37:15 +0000

    implement eqObjectsTest.js

--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,25 +1,102 @@
+const eqObjects = require("../eqObjects.js");
+const chai = require('chai');
+const assert = chai.assert;
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+describe("#eqObjects", () => {
+  it(`return true for { a: "1", b: "2" } and { b: "2", a: "1" }`, () => {
+    const input1 = { a: "1", b: "2" };
+    const input2 = { b: "2", a: "1" };
+    const expectedOutput = true;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
+  it(`return false for { a: "1", b: "2" } and { a: "1", b: "2", c: "3" }`, () => {
+    const input1 = { a: "1", b: "2" };
+    const input2 = { a: "1", b: "2", c: "3" };
+    const expectedOutput = false;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+  it(`return true for { c: "1", d: ["2", 3] } and { d: ["2", 3], c: "1" }`, () => {
+    const input1 = { c: "1", d: ["2", 3] };
+    const input2 = { d: ["2", 3], c: "1" };
+    const expectedOutput = true;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+  it(`return false for { c: "1", d: ["2", 3] } and { c: "1", d: ["2", 3, 4] }`, () => {
+    const input1 = { c: "1", d: ["2", 3] };
+    const input2 = { c: "1", d: ["2", 3, 4] };
+    const expectedOutput = false;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
 
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),false); // => false
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { y: 0, z: 1 }, b: 2 }), true); // => true
+  it(`return true for { a: { z: 1 }, b: 2 } and { a: { z: 1 }, b: 2 }`, () => {
+    const input1 = { a: { z: 1 }, b: 2 };
+    const input2 = { a: { z: 1 }, b: 2 };
+    const expectedOutput = true;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
+  
+  it(`return false for { a: { y: 0, z: 1 }, b: 2 } and { a: { z: 1 }, b: 2 }`, () => {
+    const input1 = { a: { y: 0, z: 1 }, b: 2 };
+    const input2 = { a: { z: 1 }, b: 2 };
+    const expectedOutput = false;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
 
-assertEqual(eqObjects({ a: { y: 0, z: {a: 1, b: 2} }, b: 2 }, { a: { y: 0, z: {a: 1, b: 2} }, b: 2 }), true); // => true
-assertEqual(eqObjects({ a: { y: 0, z: {a: 1, b: 2} }, b: 2 }, { a: { y: 0, z: {a: 3, b: 2} }, b: 2 }), false); // => false
+  it(`return false for { a: { y: 0, z: 1 }, b: 2 } and { a: 1, b: 2 }`, () => {
+    const input1 = { a: { y: 0, z: 1 }, b: 2 };
+    const input2 = { a: 1, b: 2 };
+    const expectedOutput = false;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
 
-assertEqual(eqObjects({ a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }}, { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }}), true); // => true
-assertEqual(eqObjects({ a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }}, { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: ".", c: "-"} }, b: 2 }}), false); // => true
+  it(`return true for { a: { y: 0, z: 1 }, b: 2 } and { a: { y: 0, z: 1 }, b: 2 }`, () => {
+    const input1 = { a: { y: 0, z: 1 }, b: 2 };
+    const input2 = { a: { y: 0, z: 1 }, b: 2 };
+    const expectedOutput = true;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
+
+  it(`return true for { a: { y: 0, z: {a: 1, b: 2} }, b: 2 } and { a: { y: 0, z: {a: 1, b: 2} }, b: 2 }`, () => {
+    const input1 = { a: { y: 0, z: {a: 1, b: 2} }, b: 2 };
+    const input2 = { a: { y: 0, z: {a: 1, b: 2} }, b: 2 };
+    const expectedOutput = true;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
+
+  it(`return false for { a: { y: 0, z: {a: 1, b: 2} }, b: 2 }, and { a: { y: 0, z: {a: 3, b: 2} }, b: 2 }`, () => {
+    const input1 = { a: { y: 0, z: {a: 1, b: 2} }, b: 2 };
+    const input2 = { a: { y: 0, z: {a: 3, b: 2} }, b: 2 };
+    const expectedOutput = false;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
+  
+  it(`return true for { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }}, and { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }`, () => {
+    const input1 = { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }};
+    const input2 = { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }};
+    const expectedOutput = true;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
+
+  it(`return true for { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }}, and { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: ".", c: "-"} }, b: 2 }}`, () => {
+    const input1 = { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: "9", c: "-"} }, b: 2 }};
+    const input2 = { a: { y: 0, z: {a: 1, b: 2, c: {a: "g", b: ".", c: "-"} }, b: 2 }};
+    const expectedOutput = false;
+    const result = eqObjects(input1, input2);
+    assert.strictEqual(result, expectedOutput);
+  });
+
+});
