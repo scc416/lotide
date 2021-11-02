f49a4aa4d75340fff4cc655b5e04c684e1d4e8e3
scc416 <siuc@protonmail.com>
2021-11-02 04:15:26 +0000

    eslint test/eqArraysTest.js

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -9,7 +9,7 @@ describe("#eqArrays", () => {
     const expectedOutput = true;
     const result = eqArrays(input1, input2);
     assert.strictEqual(result, expectedOutput);
-  })
+  });
 
   it("return false for [1, 2, 3] and [1, 2]", () => {
     const input1 = [1, 2, 3];
@@ -17,7 +17,7 @@ describe("#eqArrays", () => {
     const expectedOutput = false;
     const result = eqArrays(input1, input2);
     assert.strictEqual(result, expectedOutput);
-  })
+  });
 
   it("return false for [1, 3, 3] and [1, 2, 3]", () => {
     const input1 = [1, 3, 3];
@@ -25,15 +25,15 @@ describe("#eqArrays", () => {
     const expectedOutput = false;
     const result = eqArrays(input1, input2);
     assert.strictEqual(result, expectedOutput);
-  })
-  
+  });
+
   it("return true for [[2, 3], [4]] and [[2, 3], [4]]", () => {
     const input1 = [[2, 3], [4]];
     const input2 = [[2, 3], [4]];
     const expectedOutput = true;
     const result = eqArrays(input1, input2);
     assert.strictEqual(result, expectedOutput);
-  })
+  });
 
   it("return false for [[2, 3], [4]] and [[2, 3], 4]", () => {
     const input1 = [[2, 3], [4]];
@@ -41,7 +41,7 @@ describe("#eqArrays", () => {
     const expectedOutput = false;
     const result = eqArrays(input1, input2);
     assert.strictEqual(result, expectedOutput);
-  })
+  });
 
   it("return true for [[[2, 3], [4]], [2, 3], [4]] and [[[2, 3], [4]], [2, 3], [4]]", () => {
     const input1 = [[[2, 3], [4]], [2, 3], [4]];
@@ -49,7 +49,7 @@ describe("#eqArrays", () => {
     const expectedOutput = true;
     const result = eqArrays(input1, input2);
     assert.strictEqual(result, expectedOutput);
-  })
+  });
 
   it("return false for [[2, 3], [4, 5]] and [[2, 1], [4, 5]]", () => {
     const input1 = [[2, 3], [4, 5]];
@@ -57,7 +57,7 @@ describe("#eqArrays", () => {
     const expectedOutput = false;
     const result = eqArrays(input1, input2);
     assert.strictEqual(result, expectedOutput);
-  })
+  });
 
   it("return false for [[[2, 3], [4]], [2, 3], [4]] and [[[2, 3], [4]], [4, 3], [4]]", () => {
     const input1 = [[[2, 3], [4]], [2, 3], [4]];
@@ -65,5 +65,5 @@ describe("#eqArrays", () => {
     const expectedOutput = false;
     const result = eqArrays(input1, input2);
     assert.strictEqual(result, expectedOutput);
-  })
+  });
 });
