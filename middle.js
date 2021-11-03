6c8513c669484999f4f8a917f34844c50b13ce75
scc416 <siuc@protonmail.com>
2021-11-03 03:23:41 +0000

    eslint middle.js

--- middle.js
+++ middle.js
@@ -1,18 +1,18 @@
 
 const middle = function(array) {
   const length = array.length;
-  if (length <= 2 ) return [];
-  if (length % 2 === 0 ) {
+  if (length <= 2) return [];
+  if (length % 2 === 0) {
     const index = length / 2;
     const middleLeft = array[index - 1];
     const middleRight = array[index];
     return [middleLeft, middleRight];
-  } 
-  if (length % 2 === 1 ) {
+  }
+  if (length % 2 === 1) {
     const index = Math.floor(length / 2);
     const middle = array[index];
     return [ middle ];
   }
-}
+};
 
 module.exports = middle;
