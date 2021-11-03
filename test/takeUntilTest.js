4be2acdc8ed908e2205722c5c7893aec3fd2341c
scc416 <siuc@protonmail.com>
2021-11-03 03:51:04 +0000

    implement takeUntilTest.js

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -1,13 +1,49 @@
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-const results3 = takeUntil(data2, x => x === '');
-const results4 = takeUntil(data1, x => x % 2 === 0);
-const results5 = takeUntil(data1, x => x > 3);
+const chai = require("chai");
+const assert = chai.assert;
+const takeUntil = require("../takeUntil.js");
 
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
-assertArraysEqual(results3, ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"]);
-assertArraysEqual(results4, [1]);
-assertArraysEqual(results5, [1, 2]);
+describe("#takeUntil", () => {
+  it("return [ 1, 2, 5, 7, 2 ] for [1, 2, 5, 7, 2, -1, 2, 4, 5] and x => x < 0", () => {
+    const input = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    const callback = x => x < 0;
+    const expectedOutput = [ 1, 2, 5, 7, 2 ];
+    const result = takeUntil(input, callback);
+    console.log(result);
+    assert.deepStrictEqual(result, expectedOutput);
+  });
+
+  it(`return [ "I\'ve", "been", "to", "Hollywood" ] for ["I\'ve", "been", "to", "Hollywood", ",", "I\'ve", "been", "to", "Redwood"] and x => x === ","`, () => {
+    const input = ["I\'ve", "been", "to", "Hollywood", ",", "I\'ve", "been", "to", "Redwood"];
+    const callback = x => x === ",";
+    const expectedOutput = [ "I\'ve", "been", "to", "Hollywood" ];
+    const result = takeUntil(input, callback);
+    assert.deepStrictEqual(result, expectedOutput);
+  });
+
+  it(`return ["I\'ve", "been", "to", "Hollywood", ",", "I\'ve", "been", "to", "Redwood"] for ["I\'ve", "been", "to", "Hollywood", ",", "I\'ve", "been", "to", "Redwood"] and x => x === ""`, () => {
+    const input = ["I\'ve", "been", "to", "Hollywood", ",", "I\'ve", "been", "to", "Redwood"];
+    const callback = x => x === "";
+    const expectedOutput = ["I\'ve", "been", "to", "Hollywood", ",", "I\'ve", "been", "to", "Redwood"];
+    const result = takeUntil(input, callback);
+    assert.deepStrictEqual(result, expectedOutput);
+  });
+
+  it("return [1] for [1, 2, 5, 7, 2, -1, 2, 4, 5] and x => x % 2 === 0", () => {
+    const input = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    const callback = x => x % 2 === 0;
+    const expectedOutput = [1];
+    const result = takeUntil(input, callback);
+    console.log(result);
+    assert.deepStrictEqual(result, expectedOutput);
+  });
+
+  it("return [1, 2] for [1, 2, 5, 7, 2, -1, 2, 4, 5] and x => x > 3", () => {
+    const input = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    const callback = x => x > 3;
+    const expectedOutput = [1, 2];
+    const result = takeUntil(input, callback);
+    console.log(result);
+    assert.deepStrictEqual(result, expectedOutput);
+  });
+
+});
