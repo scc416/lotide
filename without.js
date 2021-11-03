3fe8cdda15fb9a1dd7085fa4212f076d823aea8d
scc416 <siuc@protonmail.com>
2021-11-03 04:05:56 +0000

    eslint without.js

--- without.js
+++ without.js
@@ -5,13 +5,13 @@ const without = (source, itemsToRemove) => {
       if (foundInItemsToRemove) return false;
     }
     return true;
-  }
+  };
   const arr = [];
-  for(const item of source) {
+  for (const item of source) {
     const shouldKeepItem = shouldItemBeKept(item);
     if (shouldKeepItem) arr.push(item);
   }
   return arr;
-}
+};
 
 module.exports = without;
