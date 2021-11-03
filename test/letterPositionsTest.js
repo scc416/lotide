8b4e3da52a7cba20cb6aa6e49812fdce210de8b6
scc416 <siuc@protonmail.com>
2021-11-03 03:17:06 +0000

    implement 6 test for letterPositions.js in letterPositionsTest.js

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -44,11 +44,43 @@ describe("#letterPositions", () => {
     const result = letterPositions(input).i;
     assert.deepStrictEqual(result, expectedOutput);
   });
+
+  it(`return [1, 11] for value of "i" from input "Lighthouse in the house"`, () => {
+    const input = "Lighthouse in the house";
+    const expectedOutput = [1, 11];
+    const result = letterPositions(input).i;
+    assert.deepStrictEqual(result, expectedOutput);
+  });
+
+  it(`return [2] for value of "g" from input "Lighthouse in the house"`, () => {
+    const input = "Lighthouse in the house";
+    const expectedOutput = [2];
+    const result = letterPositions(input).g;
+    assert.deepStrictEqual(result, expectedOutput);
+  });
+
+  it(`return [3, 5, 15, 18] for value of "h" from input "Lighthouse in the house"`, () => {
+    const input = "Lighthouse in the house";
+    const expectedOutput = [3, 5, 15, 18];
+    const result = letterPositions(input).h;
+    assert.deepStrictEqual(result, expectedOutput);
+  });
+
+  it(`return [4, 14] for value of "t" from input "Lighthouse in the house"`, () => {
+    const input = "Lighthouse in the house";
+    const expectedOutput = [4, 14];
+    const result = letterPositions(input).t;
+    assert.deepStrictEqual(result, expectedOutput);
+  });
+
+  it(`return [6, 19] for value of "o" from input "Lighthouse in the house"`, () => {
+    const input = "Lighthouse in the house";
+    const expectedOutput = [6, 19];
+    const result = letterPositions(input).o;
+    assert.deepStrictEqual(result, expectedOutput);
+  });
 });
 
-// assertArraysEqual(result4.g, [2]);
-// assertArraysEqual(result4.h, [3, 5, 15, 18]);
-// assertArraysEqual(result4.t, [4, 14]);
 // assertArraysEqual(result4.o, [6, 19]);
 // assertArraysEqual(result4.u, [7, 20]);
 // assertArraysEqual(result4.s, [8, 21]);
