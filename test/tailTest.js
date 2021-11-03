690ab9c190d7ee64d613d01184ef42dcd6735050
scc416 <siuc@protonmail.com>
2021-11-03 04:22:56 +0000

    eslint test/tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,14 +5,18 @@ const assert = chai.assert;
 describe("#tail", () => {
   it("returns [2, 3] for [1, 2, 3]", () => {
     assert.deepEqual(tail([1, 2, 3]), [2, 3]);
-  })
+  });
+
   it("returns [] for [1]", () => {
     assert.deepEqual(tail([1]), []);
-  })
+  });
+
   it("returns [] for []", () => {
     assert.deepEqual(tail([]), []);
-  })
+  });
+
   it(`returns ["Yo Yo", "Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"]`, () => {
     assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-  })
-})
+  });
+
+});
